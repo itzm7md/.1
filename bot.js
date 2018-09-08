ba025006ad195d08c5606342baf32eaee1bdb65a
itzm7md <41285872+itzm7md@users.noreply.github.com>
2018-09-08 14:30:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
 client.on('ready',async () => {
 let channel = hero.channels.find(r => r.id === '487045322929995777' && r.type === 'voice');
 channel.join().catch(e => console.error(e));
